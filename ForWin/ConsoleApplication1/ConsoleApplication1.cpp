9540d8c5af0c88b4912a2fa3d6a161c992d20615
deyanlaf0409 <111605228+deyanlaf0409@users.noreply.github.com>
2023-12-14 13:02:13 +0200

    Add files via upload

---
+++ ForWin/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,417 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <GL/glut.h>
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.1415926
+#define P2 PI/2
+#define P3 3*PI/2
+//#define DEG 0.0174533
+
+
+#define WINDOW_HEIGHT 512
+#define WINDOW_WIDTH 1024
+
+#define NUM_OF_RAYS 120
+#define RAY_ANGLE_INC 0.0074533
+#define MAP_SIZE 210//should be a multiple of MAP_ARRAY
+#define MAP_ARRAY 15//the size of our map in cells, maps always square
+#define MAP_CELL_SIZE   MAP_SIZE/MAP_ARRAY
+#define PLAYER_SIZE    MAP_CELL_SIZE/3
+#define VIEW_STRIP    WINDOW_WIDTH/NUM_OF_RAYS
+#define TURNING_ANGLE 0.07
+#define SPEED    PLAYER_SIZE/3
+
+
+
+
+//puts them in the middle of cell 1,1 facing right
+float playerX = MAP_CELL_SIZE * 1.5;
+float playerY = MAP_CELL_SIZE * 0.5;
+float playerAngle = 1.5;
+
+
+
+int map[] =
+{ 0,0,1,1,1,1,1,1,1,1,1,1,1,1,1
+,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1
+,1,0,0,0,1,1,0,1,0,1,1,1,1,1,1
+,1,0,1,1,1,0,0,1,0,0,0,0,0,0,1
+,1,0,1,0,0,0,1,1,1,1,1,0,1,0,1
+,1,0,1,1,1,1,1,0,0,0,1,0,1,0,1
+,1,0,0,0,1,0,0,0,1,0,1,1,1,0,1
+,1,1,1,0,1,0,1,0,1,0,1,0,0,0,1
+,1,0,0,0,1,0,1,0,1,0,1,0,1,0,1
+,1,1,1,0,1,0,1,0,1,0,0,0,1,0,1
+,1,0,0,0,1,0,1,0,1,1,1,1,1,1,1
+,1,0,1,1,1,0,1,0,0,0,0,0,0,0,1
+,1,0,1,0,0,0,1,1,1,0,1,1,1,0,1
+,1,0,0,0,1,0,0,1,0,0,1,0,0,0,0
+,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0 };
+
+
+struct Ray {
+    float distance;
+    float angle;
+    bool vertical;
+    int wallType;
+};
+
+Ray rays[NUM_OF_RAYS];
+
+
+
+
+void drawMinimap() {
+    //to put the map in the bottm right
+    float translateY = WINDOW_HEIGHT - MAP_SIZE;
+
+    //draw the maps border
+    glColor3f(0, 0, 0);
+    glBegin(GL_QUADS);
+    glVertex2f(0, translateY);
+    glVertex2f(MAP_SIZE, translateY);
+    glVertex2f(MAP_SIZE, translateY + MAP_SIZE);
+    glVertex2f(0, translateY + MAP_SIZE);
+    glEnd();
+
+    //loop through map
+    for (int i = 0; i < MAP_ARRAY; i++) {
+        for (int j = 0; j < MAP_ARRAY; j++) {
+
+            //pick colour based on wall type
+            switch (map[i * MAP_ARRAY + j]) {
+            case 0:
+                glColor3f(0, 0, 0);
+                break;
+            case 1:
+                glColor3f(255, 255, 255);
+                break;
+            }
+
+            //draw           
+            glBegin(GL_QUADS);
+            glVertex2f(MAP_CELL_SIZE * j + 1, translateY + MAP_CELL_SIZE * i + 1);
+            glVertex2f(MAP_CELL_SIZE * j + MAP_CELL_SIZE - 1, translateY + MAP_CELL_SIZE * i + 1);
+            glVertex2f(MAP_CELL_SIZE * j + MAP_CELL_SIZE - 1, translateY + MAP_CELL_SIZE * i + MAP_CELL_SIZE - 1);
+            glVertex2f(MAP_CELL_SIZE * j + 1, translateY + MAP_CELL_SIZE * i + MAP_CELL_SIZE - 1);
+            glEnd();
+
+        }
+
+    }
+
+    //draw the player, just a point with a line showing where they're facing
+    /*
+    glPointSize(PLAYER_SIZE);
+    glColor3f(0,255,0);
+
+    glBegin(GL_POINTS);
+        glVertex2f(playerX, translateY + playerY);
+    glEnd();
+
+    glLineWidth(1);
+    glBegin(GL_LINES);
+        glVertex2f(playerX,translateY + playerY);
+        glVertex2f(playerX + cos(playerAngle)*PLAYER_SIZE*2, translateY +playerY + sin(playerAngle)*PLAYER_SIZE*2);
+    glEnd();
+    */
+}
+
+
+
+
+
+
+void drawView() {
+
+    //draw the floor
+    glColor3f(0, 0, 0);
+    glBegin(GL_QUADS);
+    glVertex2f(0, WINDOW_HEIGHT / 2);
+    glVertex2f(WINDOW_WIDTH, WINDOW_HEIGHT / 2);
+    glVertex2f(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glVertex2f(0, WINDOW_HEIGHT);
+    glEnd();
+
+
+    //draw each part of the wall
+    float lineHeight;
+    float fixFishEye;
+    for (int i = 0; i < sizeof(rays) / sizeof(Ray); i++) {
+
+        //no idea how this fixes the distortion
+        fixFishEye = playerAngle - rays[i].angle;
+        if (fixFishEye < 0) {//constrain angle
+            fixFishEye += 2 * PI;
+        }
+        else if (fixFishEye > 2 * PI) {
+            fixFishEye -= 2 * PI;
+        }
+        rays[i].distance = rays[i].distance * cos(fixFishEye);
+
+        //pick a colour
+        switch (rays[i].wallType) {
+        case 1:
+            if (rays[i].vertical) {
+                glColor3f(0.6, 0.6, 0.6);
+            }
+            else {
+                glColor3f(0.4, 0.4, 0.4);
+            }
+            break;
+        }
+
+
+        //set line height and draw
+        lineHeight = (MAP_CELL_SIZE * WINDOW_HEIGHT) / rays[i].distance;
+        glBegin(GL_QUADS);
+        glVertex2f(i * VIEW_STRIP, WINDOW_HEIGHT / 2 - lineHeight / 2);
+        glVertex2f((i + 1) * VIEW_STRIP, WINDOW_HEIGHT / 2 - lineHeight / 2);
+        glVertex2f((i + 1) * VIEW_STRIP, WINDOW_HEIGHT / 2 + lineHeight);
+        glVertex2f(i * VIEW_STRIP, WINDOW_HEIGHT / 2 + lineHeight);
+        glEnd();
+    }
+}
+
+
+
+
+//helper function for casting rays
+float distance(float ax, float bx, float ay, float by) {
+    return sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay));
+}
+
+
+
+//casts a bunch o rays
+void castRays() {
+    float rayY, rayX, xOffset, yOffset;
+    int mapX, mapY, mapIndex, hWallType, vWallType;
+    int depth, maxDepth = MAP_ARRAY; //how many times we loop before giving up
+
+    float rayAngle = playerAngle - NUM_OF_RAYS / 2 * RAY_ANGLE_INC;
+    if (rayAngle < 0) {//constrain angle
+        rayAngle += 2 * PI;
+    }
+    else if (rayAngle > 2 * PI) {
+        rayAngle -= 2 * PI;
+    }
+
+
+    for (int i = 0;i < NUM_OF_RAYS; i++) {
+        //HORIZONTAL RAY CHECK
+        float hX, hY, hDistance = 9999999999;
+        depth = 0;
+        float aTan = -1 / tan(rayAngle);
+
+        //initial maths to find the ray cords and offsets
+        if (rayAngle == PI || rayAngle == 0) {//looking straight left or right, never gonna collide
+            rayX = playerX;
+            rayY = playerY;
+            depth = 8;
+        }
+        else {
+            if (rayAngle > PI) {//looking up
+                rayY = ((int)playerY) / ((int)MAP_CELL_SIZE) * ((int)MAP_CELL_SIZE) - 0.0001;
+                yOffset = -MAP_CELL_SIZE;
+            }
+            else {//looking down
+                rayY = (((int)playerY) / ((int)MAP_CELL_SIZE) * ((int)MAP_CELL_SIZE)) + MAP_CELL_SIZE;
+                yOffset = MAP_CELL_SIZE;
+            }
+            rayX = (playerY - rayY) * aTan + playerX;
+            xOffset = -yOffset * aTan;
+        }
+
+        //figure out when we hit a wall
+        while (depth < maxDepth) {
+            mapX = ((int)rayX) / ((int)MAP_CELL_SIZE);
+            mapY = rayY / ((int)MAP_CELL_SIZE);
+            mapIndex = mapY * MAP_ARRAY + mapX;
+            if (mapIndex >= 0 && mapIndex < MAP_ARRAY * MAP_ARRAY && map[mapIndex] != 0) {//hit a wall
+                depth = maxDepth;
+                hWallType = map[mapIndex];
+                hX = rayX;
+                hY = rayY;
+                hDistance = distance(playerX, hX, playerY, hY);
+            }
+            else {
+                rayX += xOffset;
+                rayY += yOffset;
+                depth++;
+            }
+        }
+
+
+
+        //VERTICAL RAY CHECK
+        //basically identidentical to horizontal check
+        float vX, vY, vDistance = 9999999999;
+        depth = 0;
+        float nTan = -tan(rayAngle);
+
+
+        //initial maths to find the ray cords and offsets
+        if (rayAngle == P2 || rayAngle == P3) {//looking straight up or down, never gonna collide
+            rayX = playerX;
+            rayY = playerY;
+            depth = 8;
+        }
+        else {
+            if (rayAngle > P2 && rayAngle < P3) {//looking up
+                rayX = ((int)playerX) / ((int)MAP_CELL_SIZE) * ((int)MAP_CELL_SIZE) - 0.0001;
+                xOffset = -MAP_CELL_SIZE;
+            }
+            else {//looking down
+                rayX = (((int)playerX) / ((int)MAP_CELL_SIZE) * ((int)MAP_CELL_SIZE)) + MAP_CELL_SIZE;
+                xOffset = MAP_CELL_SIZE;
+
+            }
+            rayY = (playerX - rayX) * nTan + playerY;
+            yOffset = -xOffset * nTan;
+        }
+
+        //figure out when we hit a wall
+        while (depth < maxDepth) {
+            mapX = ((int)rayX) / ((int)MAP_CELL_SIZE);
+            mapY = rayY / ((int)MAP_CELL_SIZE);
+            mapIndex = mapY * MAP_ARRAY + mapX;
+            if (mapIndex >= 0 && mapIndex < MAP_ARRAY * MAP_ARRAY && map[mapIndex] != 0) {//hit a wall
+                depth = maxDepth;
+                vWallType = map[mapIndex];
+                vX = rayX;
+                vY = rayY;
+                vDistance = distance(playerX, vX, playerY, vY);
+            }
+            else {
+                rayX += xOffset;
+                rayY += yOffset;
+                depth++;
+            }
+        }
+
+        //pick the smallest of the two
+        if (hDistance < vDistance) {
+            rayX = hX;
+            rayY = hY;
+            rays[i].distance = hDistance;
+            rays[i].vertical = false;
+            rays[i].wallType = hWallType;
+            rays[i].angle = rayAngle;
+        }
+        else {
+            rays[i].distance = vDistance;
+            rays[i].vertical = true;
+            rays[i].wallType = vWallType;
+            rays[i].angle = rayAngle;
+        }
+
+        //increase angle for next ray
+        rayAngle += RAY_ANGLE_INC;
+        if (rayAngle < 0) {//constrain angle
+            rayAngle += 2 * PI;
+        }
+        else if (rayAngle > 2 * PI) {
+            rayAngle -= 2 * PI;
+        }
+    }
+}
+
+
+//TODO collision detection
+//tank controls
+//not sure why we need int x and y as args
+void buttons(unsigned char key, int x, int y) {
+
+    float nextX_w, nextY_w, nextX_s, nextY_s;
+    int nextMapIndex_w, nextMapIndex_s;
+
+    switch (key) {
+    case 'w':
+        // Calculate the next position
+        nextX_w = playerX + cos(playerAngle) * SPEED;
+        nextY_w = playerY + sin(playerAngle) * SPEED;
+
+        // Check if the next position is within the map boundaries and not colliding with a wall
+        nextMapIndex_w = ((int)nextY_w / ((int)MAP_CELL_SIZE)) * MAP_ARRAY + ((int)nextX_w / ((int)MAP_CELL_SIZE));
+        if (nextMapIndex_w >= 0 && nextMapIndex_w < MAP_ARRAY * MAP_ARRAY && map[nextMapIndex_w] == 0) {
+            // Update the player's position
+            playerX = nextX_w;
+            playerY = nextY_w;
+        }
+        break;
+
+    case 's':
+        // Calculate the next position
+        nextX_s = playerX - cos(playerAngle) * SPEED;
+        nextY_s = playerY - sin(playerAngle) * SPEED;
+
+        // Check if the next position is within the map boundaries and not colliding with a wall
+        nextMapIndex_s = ((int)nextY_s / ((int)MAP_CELL_SIZE)) * MAP_ARRAY + ((int)nextX_s / ((int)MAP_CELL_SIZE));
+        if (nextMapIndex_s >= 0 && nextMapIndex_s < MAP_ARRAY * MAP_ARRAY && map[nextMapIndex_s] == 0) {
+            // Update the player's position
+            playerX = nextX_s;
+            playerY = nextY_s;
+        }
+        break;
+    case 'd':
+        playerAngle += TURNING_ANGLE;
+        if (playerAngle >= 2 * PI) { playerAngle = 0; }
+        break;
+    case 'a':
+        playerAngle -= TURNING_ANGLE;
+        if (playerAngle <= 0) { playerAngle = 2 * PI; }
+        break;
+    case 27: // 'esc' key
+        exit(0);
+        break;
+    }
+
+
+    // Refresh the display after each key press
+    glutPostRedisplay();
+}
+
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);//not sure
+    glutSwapBuffers();//not sure
+
+    castRays();
+    drawView();
+    drawMinimap();
+
+    glFlush();//not sure
+}
+
+
+void init() {
+    glutInitDisplayMode(GLUT_SINGLE);//not sure
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutCreateWindow("Dungeon");
+    glClearColor(0, 0, 0, 0);//set bg color to black
+    gluOrtho2D(0, WINDOW_WIDTH, WINDOW_HEIGHT, 0);//sets x, y co-ords
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    init();
+    glutDisplayFunc(display);
+    glutKeyboardFunc(buttons);
+    glutMainLoop();
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ ForWin/glut-3.7.6-bin/README.txt
@@ -0,0 +1,5 @@
+glut.h file in --> C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\VS\include\gl
+
+glut32.lib in --> C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\VS\lib\x86
+
+glut32.dll in --> C:\Windows\SysWOW64  and  C:\Windows\System32
